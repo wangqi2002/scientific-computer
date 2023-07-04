a55427903a530ebe54835e6ca640bc87028b2106
wangqi <1722009706@qq.com>
2023-07-04 15:58:23 +0800

    项目结构

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="option_box">
+        <div class="title_box">多用报表</div>
+        <div class="download_box">
+            <el-divider>报表导出</el-divider>
+            <button class="download_btn" @click="downloadReport">导出报表</button>
+            <button class="print_btn" @click="printReport">打印报表</button>
+        </div>
+    </div>
+</template>
+    
+<script setup>
+import { ref } from "vue";
+
+const jsonData = ref({});
+
+
+const downloadReport = () => {
+    window.print();
+};
+const printReport = () => {
+    window.print();
+};
+</script>
+    
+<style src="@/style/option.scss"  lang="scss"></style>
+<style lang="scss"></style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <el-container class="container_box">
+    <el-aside class="option_box">
+      <Sidebar></Sidebar>
+    </el-aside>
+    <el-main class="content_box">
+      hhh
+    </el-main>
+  </el-container>
 </template>
 
-<script>
-export default {
-  name: 'HomeView',
-  components: {
+<script setup>
+import { ref, provide, onMounted } from "vue";
+
+import Sidebar from "@/components/Sidebar.vue";
+
+
+onMounted(() => {
+});
+</script>
+<style lang="scss">
+$option: 250px;
+
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.container_box {
+  width: 100%;
+  height: 100%;
+
+  .option_box {
+    width: $option;
+  }
+
+  .content_box {
+    width: calc(100% - $option);
+    height: 100%;
+    padding: 0;
+    overflow: hidden;
   }
 }
-</script>
+</style>
